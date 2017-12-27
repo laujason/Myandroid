0a7bb6eff8f647b387b4a9400d94a9f80fe859f1
ormediagroup <33506028+ormediagroup@users.noreply.github.com>
2017-12-27 11:05:02 +0800

    added login activity

--- app/src/main/java/com/ormedia/qrscanner/login.java
+++ app/src/main/java/com/ormedia/qrscanner/login.java
@@ -3,6 +3,8 @@ package com.ormedia.qrscanner;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.ormedia.qrscanner.Network.JSONResponse;
@@ -17,13 +19,16 @@ public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_login);
 
         login();
     }
     public void login() {
-        new JSONResponse(this, "http://35.198.210.107/?action=login", new JSONResponse.onComplete() {
+        new JSONResponse(this, "http://35.198.210.107/applogin/?action=login", new JSONResponse.onComplete() {
             @Override
             public void onComplete(JSONObject json) {
                 Log.d("ORM", json.toString());
